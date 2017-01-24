306f5df6002ee47e046792427ef1f5d3dc6e7193
DHansell <dylankawika@gmail.com>
2017-01-24 11:12:16 -0800

    ChangeLog - 002 - Hansell & Kimberling
    Hansell - Add level select code, added Hub world
    
    Kimberling - Level Design for Hub world

---
+++ __TeddyProject/Teddy/Assets/_Scripts/LevelInteractions/js_levelWarp.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var levelName : String = "w_bedroom";
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function OnTriggerEnter (other: Collider)
+    {
+
+        Application.LoadLevel(levelName);
+
+    }
